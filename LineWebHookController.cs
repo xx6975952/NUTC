c979b2e2bf8ecd134838785d5c0a48004e575781
xx6975952 <96275798+xx6975952@users.noreply.github.com>
2022-03-22 21:39:46 +0800

    Add files via upload

---
+++ LineWebHookController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace isRock.Template
+{
+    public class LineWebHookController : isRock.LineBot.LineWebHookControllerBase
+    {
+        [Route("api/LineBotWebHook")]
+        [HttpPost]
+        public IActionResult POST()
+        {
+            var AdminUserId = "Ufc93bde0ed16937aee4a409c6a791c3f";
+
+            try
+            {
+                //設定ChannelAccessToken
+                this.ChannelAccessToken = "kmsy5rNb9yDbmqhbTodIZWndIfg2KQXjQD6HvbgZYhJzGMZ3Ad0WzxbMqpJmxTGtZrtN7m2kUmVEnCcO++KSp/HFYu1iA17INGoZl1mjrS3rTjKBx2/IjYgZSjFdHTT8YPJQKnOYitmL8adfDXslQAdB04t89/1O/w1cDnyilFU=";
+                //配合Line Verify
+                if (ReceivedMessage.events == null || ReceivedMessage.events.Count() <= 0 ||
+                    ReceivedMessage.events.FirstOrDefault().replyToken == "00000000000000000000000000000000") return Ok();
+                //取得Line Event
+                var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
+                var responseMsg = "";
+                //準備回覆訊息
+                if (LineEvent.type.ToLower() == "message" && LineEvent.message.type == "text")
+                    responseMsg = $"你說了: {LineEvent.message.text}";
+                else if (LineEvent.type.ToLower() == "message")
+                    responseMsg = $"收到 event : {LineEvent.type} type: {LineEvent.message.type} ";
+                else
+                    responseMsg = $"收到 event : {LineEvent.type} ";
+                //回覆訊息
+                this.ReplyMessage(LineEvent.replyToken, responseMsg);
+                //response OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                //回覆訊息
+                this.PushMessage(AdminUserId, "發生錯誤:\n" + ex.Message);
+                //response OK
+                return Ok();
+            }
+        }
+    }
+}
